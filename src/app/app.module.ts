c48080304733479546629a3b0ea13f76c463c5ec
Gabriel <gabriel@usemanu.com.br>
2021-01-12 02:25:54 -0300

    Aplicando Eventos no Componente do Carrinho de Compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { RestaurantService } from './restaurants/restaurant.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -39,7 +40,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    RestaurantService
+    RestaurantService,
+    ShoppingCartService
   ],
   bootstrap: [AppComponent]
 })
